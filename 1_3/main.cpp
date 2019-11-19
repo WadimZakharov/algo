fc2dc62a51e55c37f46a4c2203524ebdce2da173
WadimZakharov <39799985+WadimZakharov@users.noreply.github.com>
2019-11-19 12:16:23 +0300

    устранил замечания

--- 1_3/main.cpp
+++ 1_3/main.cpp
@@ -13,109 +13,118 @@
 //1_3. Реализовать очередь с помощью двух стеков. Использовать стек, реализованный с помощью динамического буфера.
 #include <iostream>
 #include <assert.h>
-#include <vector>
-class Stack_v{
-    public:
-     ~Stack_v();
-     // Проверка стека на пустоту
-       bool empty() const;
-     // Добавление элемента
-       void push(int value);
-     // Извлечение
-       int pop();
+class Stack_v {
+public:
+	~Stack_v();
+	// Проверка стека на пустоту
+	bool empty() const;
+	// Добавление элемента
+	void push(int value);
+	// Извлечение
+	int pop();
 
-    private:
-    int size = 1000000;
-    int *stack = new int[size];
-    int index = -1;
+private:
+	int size = 100;
+	int *stack = new int[size];
+	int index = -1;
 };
 
 
-class Queue{
-  public:
-    void push_back(int value);
-    int pop_front();
-  private:
-    Stack_v input_stack;
-    Stack_v output_stack;
-    
+class Queue {
+public:
+	void push_back(int value);
+	int pop_front();
+private:
+	Stack_v input_stack;
+	Stack_v output_stack;
+
 };
 
 bool Stack_v::empty() const
 {
-  return index == -1;
+	return index == -1;
 }
 
 void Stack_v::push(int value)
 {
-    stack[index+1] = value;
-    index ++;
+	stack[index + 1] = value;
+	index++;
+	if (index > size / 4)
+		//Выделение новой памяти
+	{
+		size = size * 2;
+		int *new_stack = new int[size];
+		memcpy(new_stack, stack, size * sizeof(int));
+		delete[] stack;
+		stack = new_stack;
+	}
+}
+
+Stack_v::~Stack_v() {
+	delete[] stack;
 }
 
 int Stack_v::pop() {
-    int res = -1;
-    if (!empty())
-    {
-    res = stack[index];
-    }
-    index --;
-    if (index < size/4)
-        //Освобождение лишней памяти
-    {
-        size = size/2;
-        int *new_stack = new int[size];
-        memcpy(new_stack, stack, size * sizeof(int));
-        delete [] stack;
-        stack = new_stack;
-    }
-    return res;
-}
-
-Stack_v::~Stack_v(){
-    delete [] stack;
+	int res = -1;
+	if (!empty())
+	{
+		res = stack[index];
+	}
+	index--;
+	if (index < size / 4)
+		//Освобождение лишней памяти
+	{
+		size = size / 2;
+		int *new_stack = new int[size];
+		memcpy(new_stack, stack, size * sizeof(int));
+		delete[] stack;
+		stack = new_stack;
+	}
+	return res;
 }
 
 
 
-void Queue::push_back(int value){
-    input_stack.push(value);
+
+void Queue::push_back(int value) {
+	input_stack.push(value);
 }
-int Queue::pop_front(){
-    if (output_stack.empty()){ //если выходной стек пустой, заполним его элементами из входного
-        while (!input_stack.empty()) {
-            output_stack.push(input_stack.pop());
-        }
-    }
-    if (output_stack.empty()){
-        return -1;
-    }
-    else{
-        return output_stack.pop();
-    }
+int Queue::pop_front() {
+	if (output_stack.empty()) { //если выходной стек пустой, заполним его элементами из входного
+		while (!input_stack.empty()) {
+			output_stack.push(input_stack.pop());
+		}
+	}
+	if (output_stack.empty()) {
+		return -1;
+	}
+	else {
+		return output_stack.pop();
+	}
 }
 
 int main() {
-    int commands_count = 0;
-    std::cin >> commands_count;
-    Queue queue;
-    for (int i = 0; i < commands_count; ++i) {
-      int command = 0;
-      int value = 0;
-      std::cin >> command >> value;
-        if (command == 2){
-            int q_value = queue.pop_front();
-            if (q_value == value){
-                continue;
-            }
-            else{
-                std::cout << "NO";
-                return 0;
-            }
-        }
-        else{
-            queue.push_back(value);
-        }
-    }
-    std::cout << "YES";
-    return 0;
+	int commands_count = 0;
+	std::cin >> commands_count;
+	Queue queue;
+	for (int i = 0; i < commands_count; ++i) {
+		int command = 0;
+		int value = 0;
+		std::cin >> command >> value;
+		if (command == 2) {
+			int q_value = queue.pop_front();
+			if (q_value == value) {
+				continue;
+			}
+			else {
+				std::cout << "NO";
+				return 0;
+			}
+		}
+		else {
+			queue.push_back(value);
+		}
+	}
+	std::cout << "YES";
+	return 0;
 }
