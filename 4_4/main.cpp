d53d0ec930c3018770248943a0ce37a612ea8b6a
WadimZakharov <39799985+WadimZakharov@users.noreply.github.com>
2019-11-19 12:22:27 +0300

    Сделал поиск к-ой статистики в отдельной функции, но не понял зачем мне класс компоратор, можно пожалуйста зачесть без него

--- 4_4/main.cpp
+++ 4_4/main.cpp
@@ -9,64 +9,71 @@
 using namespace std;
 int partition(vector<int> &vec, int left, int right)
 {
-    int n = right-left;
-    int main_ind = left + rand() % (n+1);
-    int main_el = vec[main_ind];
-    vec[main_ind] = vec[left];
-    vec[left] = main_el;
-    int i = right;
-    int j = i;
-    while (j > left)
-    {
-        while (vec[j] < vec[left])
-        {
-            j--;
-        }
+	int n = right - left;
+	int main_ind = left + rand() % (n + 1);
+	int main_el = vec[main_ind];
+	vec[main_ind] = vec[left];
+	vec[left] = main_el;
+	int i = right;
+	int j = i;
+	while (j > left)
+	{
+		while (vec[j] < vec[left])
+		{
+			j--;
+		}
 
-        if (j != left)
-        {
-            int tmp = vec[i];
-            vec[i] = vec[j];
-            vec[j] = tmp;
-            i--;
-            j--;
-        }
-    }
-    int tmp = vec[left];
-    vec[left] = vec[i];
-    vec[i] = tmp;
-    return i;
+		if (j != left)
+		{
+			int tmp = vec[i];
+			vec[i] = vec[j];
+			vec[j] = tmp;
+			i--;
+			j--;
+		}
+	}
+	int tmp = vec[left];
+	vec[left] = vec[i];
+	vec[i] = tmp;
+	return i;
 }
+
+int search_k_stat_index(vector<int> &vec, int k) {
+ 
+	int start = 0;
+	int end = vec.size() - 1;
+	int k_stat_index = partition(vec, start, end);
+
+	while (k_stat_index != k)
+	{
+
+		if (k_stat_index > k)
+		{
+			end = k_stat_index - 1;
+		}
+
+		else
+		{
+			start = k_stat_index + 1;
+		}
+		k_stat_index = partition(vec, start, end);
+	}
+	return k_stat_index;
+}
+
 int main()
 {
-    int n = 0;
-    int k = 0;
-    cin >> n;
-    cin >> k;
-    vector<int> vec = {};
-    for (int i = 0; i < n; i++) {
-        int value = 0;
-        std::cin >> value;
-        vec.push_back(value);
-    }
-    int start = 0;
-    int end = vec.size()-1;
-    int k_stat_index = partition(vec, start, end);
-
-    while(k_stat_index != k)
-    {
-        
-        if (k_stat_index > k)
-        {
-            end = k_stat_index-1;
-        }
-        
-        else
-        {
-            start = k_stat_index+1;
-        }
-        k_stat_index = partition(vec, start, end);
-    }
-    
-    cout<<vec[k_stat_index];
+	int n = 0;
+	int k = 0;
+	cin >> n;
+	cin >> k;
+	vector<int> vec = {};
+	for (int i = 0; i < n; i++) {
+		int value = 0;
+		std::cin >> value;
+		vec.push_back(value);
+	}
+	
+	int k_stat_index = search_k_stat_index(vec, k);
+	cout << vec[k_stat_index];
 }
