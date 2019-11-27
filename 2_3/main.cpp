e1346ef95490be7bbc09c2df115d5bdc5cc01c1d
WadimZakharov <39799985+WadimZakharov@users.noreply.github.com>
2019-11-27 23:52:27 +0300

    + Правило пяти

--- 2_3/main.cpp
+++ 2_3/main.cpp
@@ -9,124 +9,129 @@
 #include <iostream>
 class Heap {
 public:
-	void push(int value);
-	int pop_min();
-	int get_min() const;
-	int size() const;
-	bool empty() const;
-	~Heap();
+    void push(int value);
+    int pop_min();
+    int get_min() const;
+    int size() const;
+    bool empty() const;
+    Heap(){};
+    Heap(const Heap&) = delete;
+    Heap(Heap&&) = delete;
+    Heap& operator=(const Heap&) = delete;
+    Heap& operator=(Heap&&) = delete;
+    ~Heap();
 private:
-	std::vector<int> heap = {};
-	void swap(int i, int j);
-	void siftDown(int i);
-	void siftUp(int i);
-	static int parent(int i);
-	static int child(int i, int side);
+    std::vector<int> heap = {};
+    void swap(int i, int j);
+    void siftDown(int i);
+    void siftUp(int i);
+    static int parent(int i);
+    static int child(int i, int side);
 };
 
 
 bool Heap::empty() const
 {
-	return heap.size() == 0;
+    return heap.size() == 0;
 }
 void Heap::push(int value) {
-	heap.push_back(value);
-	siftUp(heap.size() - 1);
+    heap.push_back(value);
+    siftUp(heap.size() - 1);
 }
 
 int Heap::get_min() const {
-	if (empty()) {
-		return 1000000001;//нужно для работы программы решения задачи
-	}
-	return heap[0];
+    if (empty()) {
+        return 1000000001;//нужно для работы программы решения задачи
+    }
+    return heap[0];
 }
 
 int Heap::pop_min() {
-	if (empty()) {
-		return 1000000001;
-	}
-	int res = heap[0];
-	int new_head = heap[heap.size() - 1];
-	heap.pop_back();
-	if (!empty()) {
-		heap[0] = new_head;
-		siftDown(0);
-	}
-	return res;
+    if (empty()) {
+        return 1000000001;
+    }
+    int res = heap[0];
+    int new_head = heap[heap.size() - 1];
+    heap.pop_back();
+    if (!empty()) {
+        heap[0] = new_head;
+        siftDown(0);
+    }
+    return res;
 }
 
 
 void Heap::siftDown(int i) {
-	int left = child(i, 1);
-	int right = child(i, 2);
-	int new_min = i;
-	if (left < heap.size() && heap[left] < heap[new_min]) {
-		new_min = left;
-	}
-	if (right < heap.size() && heap[right] < heap[new_min]) {
-		new_min = right;
-	}
-	if (new_min != i) {
-		swap(i, new_min);
-		siftDown(new_min);
-	}
+    int left = child(i, 1);
+    int right = child(i, 2);
+    int new_min = i;
+    if (left < heap.size() && heap[left] < heap[new_min]) {
+        new_min = left;
+    }
+    if (right < heap.size() && heap[right] < heap[new_min]) {
+        new_min = right;
+    }
+    if (new_min != i) {
+        swap(i, new_min);
+        siftDown(new_min);
+    }
 }
 
 
 void Heap::siftUp(int i) {
-	while (heap[i] < heap[parent(i)] && i > 0) {
-		swap(i, parent(i));
-		i = parent(i);
-	}
+    while (heap[i] < heap[parent(i)] && i > 0) {
+        swap(i, parent(i));
+        i = parent(i);
+    }
 }
 
 void Heap::swap(int i, int j) {
-	int tmp = heap[i];
-	heap[i] = heap[j];
-	heap[j] = tmp;
+    int tmp = heap[i];
+    heap[i] = heap[j];
+    heap[j] = tmp;
 }
 int Heap::parent(int i) {
-	int par = (i - 1) / 2;
-	return par;
+    int par = (i - 1) / 2;
+    return par;
 }
 int Heap::child(int i, int side) {
-	//side = 1 для левового потомка, side = 2 для правого
-	int ch = 2 * i + side;
-	return ch;
+    //side = 1 для левового потомка, side = 2 для правого
+    int ch = 2 * i + side;
+    return ch;
 }
 
 int Heap::size() const {
-	return heap.size();
+    return heap.size();
 }
 
 Heap::~Heap() {
-	heap.clear();
+    heap.clear();
 }
 
 
 int count_tupiks(Heap &heap, int actual_max_count_tupik, int arrival_time, int departure_time) {
-	int count_tupik = actual_max_count_tupik;
-	while (arrival_time > heap.get_min()) {
-		int value = heap.pop_min();
-	}
-	heap.push(departure_time);
-	if (heap.size() > actual_max_count_tupik) {
-		count_tupik = heap.size();
-	}
-	return count_tupik;
+    int count_tupik = actual_max_count_tupik;
+    while (arrival_time > heap.get_min()) {
+        int value = heap.pop_min();
+    }
+    heap.push(departure_time);
+    if (heap.size() > actual_max_count_tupik) {
+        count_tupik = heap.size();
+    }
+    return count_tupik;
 }
 int main(int argc, const char * argv[]) {
-	int n = 0;
-	int count_tupik = 0;
-	Heap* heap = new Heap();
-	std::cin >> n;
-	for (int i = 0; i < n; ++i) {
-		int arrival_time = 0;
-		int departure_time = 0;
-		std::cin >> arrival_time >> departure_time;
-		count_tupik = count_tupiks(*heap, count_tupik, arrival_time, departure_time);
-	}
-	std::cout << count_tupik;
-	delete heap;
-	return 0;
+    int n = 0;
+    int count_tupik = 0;
+    Heap* heap = new Heap();
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int arrival_time = 0;
+        int departure_time = 0;
+        std::cin >> arrival_time >> departure_time;
+        count_tupik = count_tupiks(*heap, count_tupik, arrival_time, departure_time);
+    }
+    std::cout << count_tupik;
+    delete heap;
+    return 0;
 }
